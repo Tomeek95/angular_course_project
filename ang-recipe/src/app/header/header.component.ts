f1fa08ae712c2795838a19eee08edb9c5bcddfbd
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2020-10-04 10:17:34 +0200

    edited navigation
    implemented a simple navigation between recipes and shopping list

--- ang-recipe/src/app/header/header.component.ts
+++ ang-recipe/src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { features } from 'process';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
-export class HeaderComponent{
-    collapsed = true;
+export class HeaderComponent {
+  @Output() featureSelection = new EventEmitter<string>();
+
+  collapsed = true;
+
+  navigatingTo(feature: string){
+      this.featureSelection.emit(feature);
+  }
 }
