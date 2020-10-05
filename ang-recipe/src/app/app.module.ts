5f78690a66213abd385c21c07112fcfcd51650ef
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2020-10-05 16:49:13 +0200

    improved router further
    recipes can be selected with highlight
    created new recipe-edit component that handles the new and edit part of a recipe

--- ang-recipe/src/app/app.module.ts
+++ ang-recipe/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { FormsModule } from '@angular/forms';
 import { DroppdownDirective } from './shared/droppdown.directive';
 import { ShoppingListService } from './shopping-list/shoppingList.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { AppRoutingModule } from './app-routing.module';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DroppdownDirective
+    DroppdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
--- ang-recipe/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ang-recipe/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,22 +1,35 @@
 import { Recipe } from './../recipe.model';
 import { Component, Input, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-  
-  constructor(private recipseService: RecipeService) { }
+  recipe: Recipe;
+  id: number;
+
+  constructor(
+    private recipseService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipseService.getRecipe(this.id);
+    });
   }
 
-  onAddtoShoppingList(){
+  onAddtoShoppingList() {
     this.recipseService.addingIngredientToShoppingList(this.recipe.ingredients);
   }
-
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
 }
